19a4f21aac2cf4c56d386640f0a86c188e194d49
travisdotdev <travis4yusuf@gmail.com>
2026-05-08 13:05:00 +0100

    aAdded more functionality to gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,4 @@
 require('telescope').setup({})
-require('gitsigns').setup()
 require('which-key').setup({})
 require('nvim-autopairs').setup({})
 require('toggleterm').setup({
@@ -26,6 +25,48 @@ require('neo-tree').setup({
 			},
 		},
 	},
+})
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ ']c', bang = true })
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end, { desc = 'Jump to next git change' })
+
+        map('n', '[c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ '[c', bang = true })
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end, { desc = 'Jump to previous git change' })
+
+        -- Actions
+        map('v', '<leader>hs', function() gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, { desc = 'Git stage hunk' })
+        map('v', '<leader>hr', function() gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, { desc = 'Git reset hunk' })
+        map('n', '<leader>hs', gitsigns.stage_hunk,                    { desc = 'Git stage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk,                    { desc = 'Git reset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer,                  { desc = 'Git stage buffer' })
+        map('n', '<leader>hR', gitsigns.reset_buffer,                  { desc = 'Git reset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk,                  { desc = 'Git preview hunk' })
+        map('n', '<leader>hb', gitsigns.blame_line,                    { desc = 'Git blame line' })
+        map('n', '<leader>hd', gitsigns.diffthis,                      { desc = 'Git diff against index' })
+        map('n', '<leader>hD', function() gitsigns.diffthis('@') end,  { desc = 'Git diff against last commit' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame,     { desc = 'Toggle git blame line' })
+        map('n', '<leader>tD', gitsigns.preview_hunk_inline,           { desc = 'Toggle git show deleted' })
+    end,
 })
 require('snacks').setup({
     bigfile = { enabled = true },
